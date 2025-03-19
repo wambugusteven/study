4ead9236cd391dd0f0612b34faeed76d427d2c1c
Wambugu <stevenwambugu@proton.me>
2025-03-19 14:28:11 +0300

    .push variable is added and loggged

--- javasript/index.js
+++ javasript/index.js
@@ -1,8 +1,12 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
+
+let pushed = rows.push("freeCodeCamp");
 rows.push("freeCodeCamp");
+console.log(pushed);
 console.log(rows);
+
 let popped;
 rows.pop();
 console.log(popped);
