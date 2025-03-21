7ce433bd7285724edc55bce1d1bfa326dfc52242
Wambugu <stevenwambugu@proton.me>
2025-03-21 10:50:50 +0300

    a function padRow is declared that can be used in through out your application

--- javasript/index.js
+++ javasript/index.js
@@ -2,8 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow() {
+
+}
+
 for (let i = 0; i < count; i = i+ 1) {
-    rows.push(character.repeat(i));
+    rows.push(character.repeat(i+1));
 }
 
 let result = "";
