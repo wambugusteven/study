2f7bf3dae294d6e0a943c53accab1a9121d881a5
Wambugu <stevenwambugu@proton.me>
2025-03-21 11:54:56 +0300

    the function is given a name to be defined

--- javasript/index.js
+++ javasript/index.js
@@ -2,10 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-
+function padRow(name) {
+  return "Hello!";
 }
 padRow();
+const call = padRow();
+console.log(call);
 
 for (let i = 0; i < count; i = i+ 1) {
     rows.push(character.repeat(i+1));
