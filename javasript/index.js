f26af4d93e3f21edec73506d2cdd5ddeee97897a
Wambugu <stevenwambugu@proton.me>
2025-03-21 16:29:39 +0300

    a single-line comment is added to change the kind of loop

--- javasript/index.js
+++ javasript/index.js
@@ -6,8 +6,9 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber)
 }
 
-for (let i = 0; i < count; i ++) {
-  rows.push(padRow(i + 1, count))
+//change the code to a difrent kind of loop//
+for (let i = 1; i <= count; i ++) {
+  rows.push(padRow(i , count))
 }
 
 let result = ""
