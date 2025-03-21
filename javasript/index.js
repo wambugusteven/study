a6872eca3d26f781ac6811bade1e4bcf6a9f6431
Wambugu <stevenwambugu@proton.me>
2025-03-21 15:14:56 +0300

    additional operator is replaced with ++

--- javasript/index.js
+++ javasript/index.js
@@ -6,7 +6,7 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber)
 }
 
-for (let i = 0; i < count; i += 1) {
+for (let i = 0; i < count; i ++) {
   rows.push(padRow(i + 1, count))
 }
 
