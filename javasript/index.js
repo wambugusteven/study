c1c2bc5089febf130ba0438e8fddac26679c0fab
Wambugu <stevenwambugu@proton.me>
2025-04-22 14:21:42 +0300

    a while loop is included and "done" is incremented to avoid infinite loop which would lock the system

--- javasript/index.js
+++ javasript/index.js
@@ -11,12 +11,11 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count));
 }*/
 
-if ("") {
-  console.log("Condition is true");
-} else if (5 < 10) {
-  console.log("5 is less than 10");
-} else {
-  console.log("This is the else block")
+let continueLoop= false;
+let done= 0;
+
+while (continueLoop) {
+  done++
 }
 
 let result = ""
