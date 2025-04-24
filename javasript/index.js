553ce8dd9eefd155cf7d328342ba710e2fa60d8d
Wambugu <stevenwambugu@proton.me>
2025-04-24 11:54:19 +0300

    .shift method that removes the first element of an array is used

--- javasript/index.js
+++ javasript/index.js
@@ -20,6 +20,8 @@ function padRow(rowNumber, rowCount) {
 } */
 
   const numbers = [1, 2, 3];
+  const shifted = numbers.shift();
+console.log(shifted);
   const unshifted = numbers.unshift(5);
 console.log(unshifted);
 console.log(numbers);
