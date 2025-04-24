6f827850cbd08d42aa720bd65c9924888cd5f8d0
Wambugu <stevenwambugu@proton.me>
2025-04-24 12:05:53 +0300

    uncommenting is done for the first for loop

--- javasript/index.js
+++ javasript/index.js
@@ -7,9 +7,9 @@ function padRow(rowNumber, rowCount) {
 }
 
 // TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
+for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 /*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
@@ -19,12 +19,7 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count))
 } */
 
-  const numbers = [1, 2, 3];
-  const shifted = numbers.shift();
-console.log(shifted);
-  const unshifted = numbers.unshift(5);
-console.log(unshifted);
-console.log(numbers);
+  
 
 let result = ""
 
