4806bb64f907c29d5bdc05e100ae7eb74e192762
Wambugu <stevenwambugu@proton.me>
2025-04-25 11:30:14 +0300

    an if... else loop is entered to check whether the student have passed or failed

--- javasript/Gradebook app/Gradebook app.js
+++ javasript/Gradebook app/Gradebook app.js
@@ -1,6 +1,42 @@
 function getAverage(scores) {
-
-}
-
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  function getGrade(score) {
+   if (score === 100 )  {
+     return  "A++"
+   }
+   if (score >= 90 )  {
+     return  "A";
+   }
+   if (score >= 80 )  {
+     return  "B";
+   }
+   if (score >=70 )  {
+     return  "C";
+   }
+   if (score >=60 )  {
+     return  "D";
+   }
+   else {
+     return "F";
+   }
+  }
+  
+  function hasPassingGrade(score) {
+    if (score >= 60) {
+       return true
+    }
+    else {
+      return false
+    }
+  }
+  
+  console.log(getGrade(96));
+  console.log(getGrade(82));
+  console.log(getGrade(56));
