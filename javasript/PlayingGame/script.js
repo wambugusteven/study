2ac7a9fab35fa4858062f3ffbeff4ab8449f434f
Wambugu <stevenwambugu@proton.me>
2025-04-28 14:38:15 +0300

    button1 is set as the first element of an array

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -39,7 +39,7 @@ button3.onclick = fightDragon;
 
 
 function update(location) {
-  button1.innerText = "Go to store";
+  button1.innerText = location["button text"][0];
    button2.innerText = "Go to cave";
    button3.innerText = "Fight dragon";
    button1.onclick = goStore;
