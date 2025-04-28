fa2a9d13d578868a41dab9b2f6d1de3a9db0d911
Wambugu <stevenwambugu@proton.me>
2025-04-28 05:53:49 +0300

    a goTown function is created and all contents of goStore is copied to it

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -22,6 +22,16 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+  }
+
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
@@ -37,3 +47,11 @@ function goCave() {
   function fightDragon() {
     console.log("Fighting dragon.");
   }
+
+  function buyHealth() {
+
+  }
+  function buyWeapon() {
+    
+  }
+  
