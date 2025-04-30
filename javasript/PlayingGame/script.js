313efa636b2524e651262c32d6b233106c1ed2ec
Wambugu <stevenwambugu@proton.me>
2025-04-30 12:44:40 +0300

    += operator is used to add a string

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -108,6 +108,7 @@ function goCave() {
       newWeapon = weapons[currentWeaponIndex].name;
       text.innerText = "You now have a " + newWeapon + ".";
       inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: ";
     }
   }
  
