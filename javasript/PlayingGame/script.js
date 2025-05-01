5e7d2aded37e4f7bd6a55e0e225e8522c4cacc14
Wambugu <stevenwambugu@proton.me>
2025-05-01 12:29:40 +0300

    else part of the if loop is added to pop a message once the condition is met

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -113,6 +113,8 @@ function goCave() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
   }
  
