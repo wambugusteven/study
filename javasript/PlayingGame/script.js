5ff553e13eca6e2d3dc37649c3ac58ee1d1ea0d7
Wambugu <stevenwambugu@proton.me>
2025-05-05 09:17:36 +0300

    goFight function is updated using .innerText method

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -155,6 +155,8 @@ function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = 'block';
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
