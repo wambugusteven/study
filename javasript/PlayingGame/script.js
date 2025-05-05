747d95743de478ad5ac2ebfac2f4d7ea14adc993
Wambugu <stevenwambugu@proton.me>
2025-05-05 08:59:51 +0300

    another array is added in the location for fighting

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -57,7 +57,13 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
-  }
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+   "button functions": [attack, dodge, goTown],
+   text: "You are fighting a monster."
+   }
 ];
 
 // initialize buttons
@@ -148,3 +154,10 @@ function fightBeast() {
 function goFight() {
   
 }
+
+function attack() {
+
+};
+function dodge() {
+
+};
