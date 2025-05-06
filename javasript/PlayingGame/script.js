8183d08c415906f9e590b92a6f77e7897febd28e
Wambugu <stevenwambugu@proton.me>
2025-05-06 12:59:36 +0300

    an update object is added in the lose function

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -78,6 +78,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -191,5 +192,5 @@ function defeatMonster() {
   update(locations[4]);
 };
 function lose() {
-
+  update(locations[5]);
 };
