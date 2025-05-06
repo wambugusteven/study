1c9fc5914d6f9dbd39942840efe0b07a9409b5bb
Wambugu <stevenwambugu@proton.me>
2025-05-06 10:55:02 +0300

    addition assingment operator is used to add a text in combination with text.innertext method

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -160,6 +160,8 @@ function goFight() {
 }
 
 function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
 
 };
 function dodge() {
