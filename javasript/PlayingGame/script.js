48f1e433b48cda6564fa7e1cf12370ddaa0765de
Wambugu <stevenwambugu@proton.me>
2025-05-06 11:03:09 +0300

    monsters health should be reduced in accordance to the power of the weapon

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -162,7 +162,8 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeaponIndex].power;
 };
 function dodge() {
 
