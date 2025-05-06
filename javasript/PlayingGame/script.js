9e9ecab8bffe5a7a92dcb8b843bed5d1c6d54915
Wambugu <stevenwambugu@proton.me>
2025-05-06 10:58:19 +0300

    a new expression is created that allows to check the new health status

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -162,7 +162,7 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-
+  health -= monsters[fighting].level
 };
 function dodge() {
 
