b6e7d9df32c0d0c88a67d7dce5d94e47e2563513
Wambugu <stevenwambugu@proton.me>
2025-05-07 04:46:59 +0300

    the health expression is modified

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -181,7 +181,7 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-  health -= monsters[fighting].level;
+  health -= getMonsterAttackValue(monsters[fighting].level);
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
