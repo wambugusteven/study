634650f1812727bc21599f4c9621328737b7f292
Wambugu <stevenwambugu@proton.me>
2025-05-07 05:07:12 +0300

    an if block is added in the attacking function

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -182,19 +182,21 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;    
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
     lose();
-  }  else if (monsterHealth <= 0) {
+  } else if (monsterHealth <= 0) {
     if (fighting === 2) {
       winGame();
     } else {
       defeatMonster();
     }
   }
-};
+}
 
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
