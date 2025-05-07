e21c46352a8093199889e44bfdcb08bc7a3a45c0
Wambugu <stevenwambugu@proton.me>
2025-05-07 05:39:11 +0300

    AND (&&) oparator and not equal to (!==) is used in the code

--- javasript/PlayingGame/script.js
+++ javasript/PlayingGame/script.js
@@ -198,7 +198,7 @@ function attack() {
       defeatMonster();
     }
   }
-  if (Math.random() <= .1) {
+  if (Math.random()<= .1 && inventory.length !== 1) {
     text.innerText += " Your " + inventory.pop() + " breaks.";
     currentWeaponIndex--;
   }
