5dec4e6bd76b27bce1afe1711efdf31e891e3d8e
Wambugu <stevenwambugu@proton.me>
2025-05-08 09:31:03 +0300

    a quersy selector is used to reference to a span element in the html with an id of bg-hex-code

--- javasript/Random Background Color Changer/script.js
+++ javasript/Random Background Color Changer/script.js
@@ -6,3 +6,6 @@ function getRandomIndex() {
   getRandomIndex();
 
   const body = document.querySelector("body");
+  const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+
+console.log(bgHexCodeSpanElement);
