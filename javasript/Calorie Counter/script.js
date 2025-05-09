9ab1201c5eeacd79d4b50d594e6ed1c67781ab1d
Wambugu <stevenwambugu@proton.me>
2025-05-09 13:58:50 +0300

    a new variable is added to an empty template literal string

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -19,4 +19,5 @@ function isInvalidInput(str) {
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = ``;
 }
