6aeee0676cfa2c08106cf5d6271a49e3694a8d10
Wambugu <stevenwambugu@proton.me>
2025-05-09 14:19:05 +0300

    a second label element is included with it's properties considered

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -22,5 +22,6 @@ function addEntry() {
   const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label> 
     <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
    `;
 }
