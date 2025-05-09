5715ecd0edb05425e1b58a409acff5398089e48f
Wambugu <stevenwambugu@proton.me>
2025-05-09 14:01:47 +0300

    a label element is added inside the empty template literal

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -19,5 +19,7 @@ function isInvalidInput(str) {
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = ``;
+  const HTMLString = `
+    <label>Entry ${entryNumber} Name</label>  
+   `;
 }
