196d23e7f804aa6a3cb0bd55aca85af4567ff8bf
Wambugu <stevenwambugu@proton.me>
2025-05-10 12:51:08 +0300

    several errors in the code are debbuged

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -20,12 +20,16 @@ function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
-    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label> 
-    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
-    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
-    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />
-   `;
-   targetInputContainer.innerHTML += HTMLString;
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.innerHTML += HTMLString;
 }
 
 addEntryButton.addEventListener("click", addEntry);
