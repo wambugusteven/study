d42482238d61cf1885c06ed5ab6a2ec36be24228
Wambugu <stevenwambugu@proton.me>
2025-05-10 13:48:35 +0300

    an if loop is entered inside the for..of loop to check whether it is truthy or falsy

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -37,6 +37,9 @@ function getCaloriesFromInputs(list) {
 for (const item of list) {
   const currVal = cleanInputString(item.value);
   const invalidInputMatch = isInvalidInput(currVal);
+  if (invalidInputMatch) {
+
+  }
 }
 
 }
