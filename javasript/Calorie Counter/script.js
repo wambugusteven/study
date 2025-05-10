7e8ddb28c51b53d75a9c8023ba18e16182a536df
Wambugu <stevenwambugu@proton.me>
2025-05-10 15:59:35 +0300

    a string is declared and an argument given

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -40,6 +40,9 @@ function calculateCalories(e) {
   const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
     const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
      const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+
+     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+
 }
 
 function getCaloriesFromInputs(list) {
