8cdf3d58c59e3cccb2ddf948a79f80969df75acb
Wambugu <stevenwambugu@proton.me>
2025-05-10 13:45:43 +0300

    arguments in the for..of loop is entered

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -35,6 +35,8 @@ function addEntry() {
 function getCaloriesFromInputs(list) {
   let calories = 0;
 for (const item of list) {
+  const currVal = cleanInputString(item.value);
+  const invalidInputMatch = isInvalidInput(currVal);
 }
 
 }
