422855274cfbaf122c57698c0c1fa7c23162d6cc
Wambugu <stevenwambugu@proton.me>
2025-05-10 15:28:59 +0300

    the if loop (isError ) is set to true and return to null

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -39,6 +39,8 @@ for (const item of list) {
   const invalidInputMatch = isInvalidInput(currVal);
   if (invalidInputMatch) {
     alert(`Invalid Input: ${invalidInputMatch[0]}`);
+    isError = true;
+    return null;
   }
 }
 
