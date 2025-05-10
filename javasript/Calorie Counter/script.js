4ddf23863139370b7834264eda2be010848912ef
Wambugu <stevenwambugu@proton.me>
2025-05-10 15:32:27 +0300

    additional assingment operator is used to add to the total calculations

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -34,15 +34,18 @@ function addEntry() {
 
 function getCaloriesFromInputs(list) {
   let calories = 0;
-for (const item of list) {
-  const currVal = cleanInputString(item.value);
-  const invalidInputMatch = isInvalidInput(currVal);
-  if (invalidInputMatch) {
-    alert(`Invalid Input: ${invalidInputMatch[0]}`);
-    isError = true;
-    return null;
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
   }
-}
 
 }
 
