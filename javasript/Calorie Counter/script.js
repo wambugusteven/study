afd6e56571d2f4f33570e2ead087ffde24d2c5ab
Wambugu <stevenwambugu@proton.me>
2025-05-12 15:29:43 +0300

    toLowerCase() method is added in the template literal

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -35,28 +35,30 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
+
   const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
   const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
   const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
-    const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
-     const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+  const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
+  const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
 
-     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
-     const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
-     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
-     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
-     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-  
-     if (isError) {
-      return;
-    }
- 
-    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
-    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-     
-    output.innerHTML = ``;
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 
+  if (isError) {
+    return;
+  }
+
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+     <span class="${surplusOrDeficit.toLowerCase()}"></span>
+  `;
 }
 
 function getCaloriesFromInputs(list) {
@@ -73,7 +75,7 @@ function getCaloriesFromInputs(list) {
     }
     calories += Number(currVal);
   }
-  return  calories;
+  return calories;
 }
 
 addEntryButton.addEventListener("click", addEntry);
