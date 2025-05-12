444ff1b7efde6b30682be4fea67b9e127beeb149
Wambugu <stevenwambugu@proton.me>
2025-05-12 16:23:03 +0300

    a p element is added below the hr element and an interpolation is included

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -59,6 +59,7 @@ function calculateCalories(e) {
   output.innerHTML = `
      <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
        <hr>
+        <p>${budgetCalories} Calories Budgeted</p>
   `;
 }
 
