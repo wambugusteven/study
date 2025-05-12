fce70549b7a2fb4dc5abcc20b5c8b1405e70128c
Wambugu <stevenwambugu@proton.me>
2025-05-12 17:55:17 +0300

    a for..of loop is entered inside the clearForm

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -85,6 +85,9 @@ function getCaloriesFromInputs(list) {
 
 function clearForm() {
   const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+  for (const container of inputContainers) {
+    container.innerHTML = '';
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
