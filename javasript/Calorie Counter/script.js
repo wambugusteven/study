0a53b108bedd6f0ab094f68929a1b3093400e2a8
Wambugu <stevenwambugu@proton.me>
2025-05-12 15:19:53 +0300

    an empty template literal is added at the end of calculateCalories function

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -54,7 +54,8 @@ function calculateCalories(e) {
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-   
+     
+    output.innerHTML = ``;
 
 }
 
