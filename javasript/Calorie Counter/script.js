43ad3b6e2a5691690c464bf6887b9d16666ce03b
Wambugu <stevenwambugu@proton.me>
2025-05-12 17:58:06 +0300

    the value property of budgetNumberInput is set to an empty string

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -88,6 +88,8 @@ function clearForm() {
   for (const container of inputContainers) {
     container.innerHTML = '';
   }
+
+  budgetNumberInput.value = '';
 }
 
 addEntryButton.addEventListener("click", addEntry);
