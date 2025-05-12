15aeec0994f614fbb0822b1206864e820a2587e0
Wambugu <stevenwambugu@proton.me>
2025-05-12 10:32:14 +0300

    the rest of the strings are declered and arguments added

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -42,6 +42,11 @@ function calculateCalories(e) {
      const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
 
      const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+     const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
 
 }
 
