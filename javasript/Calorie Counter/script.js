fbee6ff463e945541b330e9fb74c0a54fbf7f668
Wambugu <stevenwambugu@proton.me>
2025-05-12 15:16:14 +0300

    a ternary opertor is used to check the calories used

--- javasript/Calorie Counter/script.js
+++ javasript/Calorie Counter/script.js
@@ -53,6 +53,8 @@ function calculateCalories(e) {
  
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+   
 
 }
 
