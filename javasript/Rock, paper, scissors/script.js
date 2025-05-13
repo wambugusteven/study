8da47df14487449410ecf6d313e8401d47906721
Wambugu <stevenwambugu@proton.me>
2025-05-13 13:00:50 +0300

    a function that checks between computer and player who have won and awards marks respectively is set

--- javasript/Rock, paper, scissors/script.js
+++ javasript/Rock, paper, scissors/script.js
@@ -12,5 +12,19 @@ function getRandomComputerResult() {
     );
   }
   
-  console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
-  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+  
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+      playerScore++;
+      return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+      return `It's a tie! Both chose ${userOption}`;
+    } else {
+      computerScore++;
+      return `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+  }
