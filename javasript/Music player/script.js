1caf06af7016a86647c4c614f462ced8274a1eec
Wambugu <stevenwambugu@proton.me>
2025-05-13 13:53:08 +0300

    second object added below the first one

--- javasript/Music player/script.js
+++ javasript/Music player/script.js
@@ -13,5 +13,12 @@ const allSongs = [
       artist: "Quincy Larson",
       duration: "4:25",
       src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+      },
+      {
+        id: 1,
+        title: "Can't Stay Down",
+         artist: "Quincy Larson",
+         duration: "4:15",
+         src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
       }
 ];
