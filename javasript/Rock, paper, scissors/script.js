fc22cf6b13b72271ff4f3e6de0bf0eb73fe3ea26
Wambugu <stevenwambugu@proton.me>
2025-05-13 12:47:36 +0300

    the riteria for the player to win a round is set

--- javasript/Rock, paper, scissors/script.js
+++ javasript/Rock, paper, scissors/script.js
@@ -1,7 +1,16 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
-  return options[randomIndex];
-  
+    return options[randomIndex];
   }
-  console.log(getRandomComputerResult());
+  
+  function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Rock" && computer === "Scissors") ||
+      (player === "Scissors" && computer === "Paper") ||
+      (player === "Paper" && computer === "Rock")
+    );
+  }
+  
+  console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
