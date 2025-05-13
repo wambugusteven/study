71e400c3a0a821cd948ad1f7327eef12796cefe0
Wambugu <stevenwambugu@proton.me>
2025-05-13 12:31:41 +0300

    a function that will generate a random choice for the computer and return the array option is created

---
+++ javasript/Rock, paper, scissors/script.js
@@ -0,0 +1,7 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+  
+  }
+  console.log(getRandomComputerResult());
