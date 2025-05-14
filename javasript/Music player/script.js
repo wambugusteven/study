a9e771dba202d51744979dd4b107a19f7d0b1303
Wambugu <stevenwambugu@proton.me>
2025-05-14 15:56:23 +0300

    an implicit return is used to replace the normal return method

--- javasript/Music player/script.js
+++ javasript/Music player/script.js
@@ -98,7 +98,7 @@ const printMessage = org => {
 
 printMessage('freeCodeCamp');
 
-const addTwoNumbers = (num1, num2) => {
-  return num1 + num2;
-}
+
+const addTwoNumbers = (num1, num2) => num1 + num2;
+
 console.log(addTwoNumbers(3, 4));
