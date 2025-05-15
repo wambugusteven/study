4480d6629e97aac3fda8a6fa2690a0fbe0769909
Wambugu <stevenwambugu@proton.me>
2025-05-15 16:03:26 +0300

    eventlistener is used to upload a text in the pause button

--- javasript/Music player/script.js
+++ javasript/Music player/script.js
@@ -138,6 +138,8 @@ playButton.addEventListener("click", () => {
   }
 });
 
+pauseButton.addEventListener("click", pauseSong);
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
