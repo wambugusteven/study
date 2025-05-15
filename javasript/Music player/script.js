34a5834fb7eff412bc24b7392f0c9e55625c3d88
Wambugu <stevenwambugu@proton.me>
2025-05-15 14:50:22 +0300

    add method is used to add a text to the play button that will display playing and a play method is used to play mp3 files

--- javasript/Music player/script.js
+++ javasript/Music player/script.js
@@ -97,6 +97,8 @@ const playSong = (id) => {
     audio.currentTime = userData?.songCurrentTime;
   }
   userData.currentSong = song;
+  playButton.classList.add("playing");
+   audio.play();
 }
 
 const renderSongs = (array) => {
