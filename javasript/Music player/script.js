45c4410103bb75be15bd60642c15dc54d0818c30
Wambugu <stevenwambugu@proton.me>
2025-05-15 15:04:10 +0300

    an else statement is added that will ensure the current song continues to play when the play button is clicked

--- javasript/Music player/script.js
+++ javasript/Music player/script.js
@@ -127,6 +127,8 @@ playButton.addEventListener("click", () => {
   if (userData?.currentSong === null) {
   
     playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
   }
 });
 
