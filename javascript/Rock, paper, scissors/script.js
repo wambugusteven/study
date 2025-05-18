d81d4e9b18b6ea033fca51165a435e33121fb98f
Wambugu <stevenwambugu@proton.me>
2025-05-18 17:22:01 +0300

    reset function ment to reset the game once player or computer wins the game is set

--- javascript/Rock, paper, scissors/script.js
+++ javascript/Rock, paper, scissors/script.js
@@ -52,8 +52,15 @@ function showResults(userOption) {
 
 };
 function resetGame() {
-
-};
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore;
+  resetGameBtn.style.display = "none";
+  optionsContainer.style.display = "block";
+  roundResultsMsg.innerText = "";
+  winnerMsgElement.innerText = "";
+  };
 
 resetGameBtn.addEventListener("click", resetGame);
 
