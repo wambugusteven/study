d7295b8fafbdbc8b976fc283062979785b6110ab
Wambugu <stevenwambugu@proton.me>
2025-05-21 11:22:38 +0300

    a function that will play previuos song is declared and if there is no current playing song, then the function will exit

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -117,7 +117,12 @@ const playNextSong = () => {
    } 
  
 };
-
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) return
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+  }
+};
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
