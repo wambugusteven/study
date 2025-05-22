17403f81b9f8e5230232b6ca9f1d1bf4a93439d4
Wambugu <stevenwambugu@proton.me>
2025-05-22 14:21:49 +0300

    a remove attribute is used to remove the aria-current

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -133,7 +133,7 @@ const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight= document.getElementById(`song-${userData?.currentSong?.id}`);
   playlistSongElements.forEach((songEl) => {
-
+    songEl.removeAttribute("aria-current");
   });
 };
 
