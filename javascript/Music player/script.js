91bbc1b8d4591a06aa7fbe253e920c40f01e0e71
Wambugu <stevenwambugu@proton.me>
2025-05-22 14:06:58 +0300

    an arrow syntax is used to write a code that will highlight the current playing song

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -103,32 +103,42 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
+  
   playButton.classList.remove("playing");
   audio.pause();
-}
+};
 
 const playNextSong = () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
-    const nextSong =  userData?.songs[currentSongIndex + 1];
+    const nextSong = userData?.songs[currentSongIndex + 1];
+
     playSong(nextSong.id);
-   } 
- 
-};
-const playPreviousSong = () => {
-  if (userData?.currentSong === null) return
-  else {
-    const currentSongIndex = getCurrentSongIndex();
   }
 };
+
+const playPreviousSong = () => {
+   if (userData?.currentSong === null) return;
+   else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+   }
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info" onclick="playSong(${song.id})>
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -145,21 +155,20 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-const getCurrentSongIndex = () => {
-  return userData?.songs.indexOf(userData?.currentSong);
-};
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
-  if (userData?.currentSong === null) {
-  
+    if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
   } else {
     playSong(userData?.currentSong.id);
   }
 });
 
-pauseButton.addEventListener("click", pauseSong);
+pauseButton.addEventListener("click",  pauseSong);
+
 nextButton.addEventListener("click", playNextSong);
+
 previousButton.addEventListener("click", playPreviousSong);
 
 const sortSongs = () => {
