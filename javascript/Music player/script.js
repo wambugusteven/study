02d4a059a60e0c1bff27cb10a70ba97836520a6c
Wambugu <stevenwambugu@proton.me>
2025-05-23 12:52:31 +0300

    current song and current song time are set respectively

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -134,6 +134,8 @@ const playPreviousSong = () => {
 };
 const shuffle = () => {
   userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong  = null;
+userData.songCurrentTime = 0;
 }  
 
 const setPlayerDisplay = () => {
