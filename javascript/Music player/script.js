d2a004ffce35ce4a8dd2013b3dfa66c22ba2e136
Wambugu <stevenwambugu@proton.me>
2025-05-23 14:01:15 +0300

    an empty delete function is passed with id as the parameter

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -143,6 +143,9 @@ const shuffle = () => {
   setPlayButtonAccessibleText();
 };
 
+const deleteSong = (id) => {
+
+} 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
