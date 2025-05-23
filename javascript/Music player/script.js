fbeac277d52e9714a1177cd029a144f5cb81c698
Wambugu <stevenwambugu@proton.me>
2025-05-23 11:23:00 +0300

    textContent that sets the text of a node and allows to set or retrieve the text content of an HTML element is included

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
+  
   audio.play();
 };
 
@@ -135,7 +136,9 @@ const setPlayerDisplay = () => {
   const songArtist = document.getElementById("player-song-artist");
   const currentTitle = userData?.currentSong?.title;
   const currentArtist = userData?.currentSong?.artist;
-  
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
