e354c58b0ae30e01a1c37d215b777dc27cb42490
Wambugu <stevenwambugu@proton.me>
2025-05-27 00:20:04 +0300

    nextSongExists constant is created and a boolean value is added

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -217,7 +217,9 @@ previousButton.addEventListener("click", playPreviousSong);
 shuffleButton.addEventListener("click", shuffle);
 
 audio.addEventListener("ended", () => {
-  
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
 });
 
 const sortSongs = () => {
