da14c9d6624d48deaf0cae6844b5abb864226be3
Wambugu <stevenwambugu@proton.me>
2025-05-26 23:57:43 +0300

    an event listener method is added to the reset button that will return the deleted song once pressed

--- javascript/Music player/script.js
+++ javascript/Music player/script.js
@@ -212,7 +212,10 @@ const renderSongs = (array) => {
     resetButton.appendChild(resetText);
     playlistSongs.appendChild(resetButton);
 
+    resetButton.addEventListener("click", () => {
   
+    });
+
   }
 
 };
