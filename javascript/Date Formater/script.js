1e8582f9a4888d061518579f5164f18137e61f7c
Wambugu <stevenwambugu@proton.me>
2025-05-30 22:32:05 +0300

    the format that includes hours and minutes displaying in the screen is eatablished inside the interceptions

--- javascript/Date Formater/script.js
+++ javascript/Date Formater/script.js
@@ -22,8 +22,8 @@ dateOptionsSelectElement.addEventListener("change", () => {
           currentDateParagraph.textContent = formattedDate.split('-').reverse().join('-');
           break;
           case "mm-dd-yyyy-h-mm":
-            currentDateParagraph.textContent = ``;
-            break;
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
     }
     
         });
