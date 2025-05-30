e2a75df462301c93196fef8d3a252921824ee6bc
Wambugu <stevenwambugu@proton.me>
2025-05-30 15:21:17 +0300

    date method with a new operator is added that is ment to display the current date and time

---
+++ javascript/Date Formater/script.js
@@ -0,0 +1,6 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+
+const day = date.getDate();
