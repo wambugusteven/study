f74535c6e04ecee8188d8b02a1d564ce7588e31b
Wambugu <stevenwambugu@proton.me>
2025-05-31 15:03:14 +0300

    a ternary operaor is used to check whether the player is the captain

--- javascript/Football Team Cards/script.js
+++ javascript/Football Team Cards/script.js
@@ -187,7 +187,7 @@ const setPlayerCards = (arr = players) => {
     ({ name, position, number, isCaptain, nickname }) => {
      return `
         <div class="player-card">
-      
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
         </div>
       `;
     }
