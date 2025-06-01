d8e15b3cbb3a25abcbb926756a6b7d531c1b5926
Wambugu <stevenwambugu@proton.me>
2025-06-01 08:49:40 +0300

    an addEventListener is added to the cancel button with a close() that will close the modal method

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -20,3 +20,7 @@ openTaskFormBtn.addEventListener("click", () =>
 closeTaskFormBtn.addEventListener("click", () => 
     confirmCloseDialog.showModal()
 );
+
+cancelBtn.addEventListener("click", () => 
+    confirmCloseDialog.close()
+);
