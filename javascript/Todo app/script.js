2592b7ebd140f562b160dd0c301a7081be06c8ac
Wambugu <stevenwambugu@proton.me>
2025-06-02 14:37:08 +0300

    a div element is added to the twmplate strings inputed

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -45,7 +45,7 @@ taskForm.addEventListener("submit", (e) => {
   }  
   taskData.forEach(({id, title, date, description}) => {
     tasksContainer.innerHTML += `
-        
+        <div class="task" id="${id}"></div>
     `
   }
 );
