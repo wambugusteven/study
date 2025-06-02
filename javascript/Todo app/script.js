3bd5fe7d42b92733b051b92c275ce975878a3cd7
Wambugu <stevenwambugu@proton.me>
2025-06-02 09:55:21 +0300

    other properties are retrived and saved in the taskObj object

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -35,7 +35,9 @@ taskForm.addEventListener("submit", (e) => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
   console.log(taskObj);
 });
