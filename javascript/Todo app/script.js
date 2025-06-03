62c7ef0b3b1f93585eae1d72474d856e69f038e9
Wambugu <stevenwambugu@proton.me>
2025-06-03 16:09:09 +0300

    delete and edit buttons are added onclick to delete and edit the task respectively

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -39,8 +39,8 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button> 
+          <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button> 
         </div>
       `
     }
