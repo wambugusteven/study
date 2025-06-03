71705bf0a39d643bb72f92df27e538a0ab804616
Wambugu <stevenwambugu@proton.me>
2025-06-03 15:48:16 +0300

    another arrow function is added and several methods moved into it

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -25,11 +25,25 @@ const addOrUpdateTask = () => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
+
+
 };
 
 const updateTaskContainer = () => {
-  
-}
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `
+    } 
+  );
+};
 
 const reset = () => {
   titleInput.value = "";
@@ -61,20 +75,6 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  taskData.forEach(
-    ({ id, title, date, description }) => {
-        tasksContainer.innerHTML += `
-        <div class="task" id="${id}">
-          <p><strong>Title:</strong> ${title}</p>
-          <p><strong>Date:</strong> ${date}</p>
-          <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
-        </div>
-      `
-    }
-  );
 
   reset()
 });
