9762291e22eadd75f1e3f273d0a50fecafe1bc07
Wambugu <stevenwambugu@proton.me>
2025-06-03 16:06:45 +0300

    the problem of duplication of the previous task when there is need to add a new one is fixed

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -25,13 +25,13 @@ const addOrUpdateTask = () => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
-   
-  updateTaskContainer();
-  reset();
-
+  updateTaskContainer()
+  reset()
 };
 
 const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+
   taskData.forEach(
     ({ id, title, date, description }) => {
         tasksContainer.innerHTML += `
@@ -40,10 +40,10 @@ const updateTaskContainer = () => {
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
           <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
+          <button type="button" class="btn">Delete</button> 
         </div>
       `
-    } 
+    }
   );
 };
 
@@ -78,5 +78,5 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  reset()
+  addOrUpdateTask();
 });
