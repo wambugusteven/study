530fad3130f1259166c351f1c8a73b2bfcab7d45
Wambugu <stevenwambugu@proton.me>
2025-06-04 14:15:43 +0300

    square bracket notation is used to retrieve the task to be edited from the taskData array using the dataArrIndex

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -59,6 +59,7 @@ const editTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+  currentTask = taskData[dataArrIndex];
 };
 
 const reset = () => {
