93750dfcbec58a0eb99c2b233178fca6df949265
Wambugu <stevenwambugu@proton.me>
2025-06-04 16:43:17 +0300

    a constant is declared and an if statement used to check whether the user has editted anything so that it does not display the close/discard buttons

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -26,6 +26,7 @@ const addOrUpdateTask = () => {
     taskData.unshift(taskObj);
   } else {
     taskData[dataArrIndex] = taskObj;
+  }
 
   updateTaskContainer()
   reset()
@@ -41,33 +42,38 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn" onclick="editTask(this)">Edit</button>
-          <button type="button" class="btn" onclick="deleteTask(this)">Delete</button> 
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `
     }
   );
 };
 
+
 const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
-};
+}
 
 const editTask = (buttonEl) => {
-  const dataArrIndex = taskData.findIndex(
+    const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
   currentTask = taskData[dataArrIndex];
+
   titleInput.value = currentTask.title;
   dateInput.value = currentTask.date;
   descriptionInput.value = currentTask.description;
+
   addOrUpdateTaskBtn.innerText = "Update Task";
 
-  taskForm.classList.toggle("hidden");
+  taskForm.classList.toggle("hidden");  
 }
 
 const reset = () => {
@@ -84,6 +90,8 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
   if (formInputsContainValues) {
     confirmCloseDialog.showModal();
   } else {
