259ec96d686bcbd037631744163b5e1bdaf880d7
Wambugu <stevenwambugu@proton.me>
2025-06-05 13:27:18 +0300

    unnecessary code is removed

--- javascript/Todo app/script.js
+++ javascript/Todo app/script.js
@@ -112,16 +112,3 @@ taskForm.addEventListener("submit", (e) => {
   addOrUpdateTask();
 });
 
-const myTaskArr = [
-  { task: "Walk the Dog", date: "22-04-2022" },
-  { task: "Read some books", date: "02-11-2023" },
-  { task: "Watch football", date: "10-08-2021" },
-];
-
-localStorage.setItem("data", JSON.stringify(myTaskArr));
-
-const getTaskArr = localStorage.getItem("data")
-console.log(getTaskArr);
-
-const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
-console.log(getTaskArrObj);
