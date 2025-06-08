0f8a8d767c33c6295f3459f6c433230bc825cac7
Wambugu <stevenwambugu@proton.me>
2025-06-08 19:01:37 +0300

    an if statement is added that checks whether the input is 0 then displays 0

--- javascript/Decimal Binary Converter/script.js
+++ javascript/Decimal Binary Converter/script.js
@@ -7,6 +7,11 @@ const decimalToBinary = (input) => {
     const quotients = [];
     const remainders = [];
 
+    if (input === 0) {
+        result.innerText = "0";
+        return;
+    } 
+
     while (input > 0) {
         const quotient = Math.floor(input / 2);
         const remainder = input % 2;
