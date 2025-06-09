8a93c7af164b4d81b0cb710559fd81ba7bb759f5
Wambugu <stevenwambugu@proton.me>
2025-06-09 14:08:36 +0300

    three functions a,b and c are created that returns the respective string joined together

--- javascript/Decimal Binary Converter/script.js
+++ javascript/Decimal Binary Converter/script.js
@@ -1,3 +1,17 @@
+const a = () => {
+  return "freeCodeCamp " + b();
+};
+
+const b = () => {
+  return "is " + c();
+};
+
+const c = () => {
+  return "awesome!";
+};
+
+console.log(a());
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
