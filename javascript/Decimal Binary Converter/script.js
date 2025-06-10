8a66957c151c7e52c4067976b3e9b6308f2030be
Wambugu <stevenwambugu@proton.me>
2025-06-10 14:13:04 +0300

    function name is updated

--- javascript/Decimal Binary Converter/script.js
+++ javascript/Decimal Binary Converter/script.js
@@ -2,13 +2,17 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countdown = (number) => {
+const countDownAndUp = (number) => {
   console.log(number);
 
   if (number === 0) {
     return;
- };  
+  } else {
+    countDownAndUp(number - 1);
+  }
 };
+
+countDownAndUp(3);
 
 const decimalToBinary = (input) => {
   let binary = "";
