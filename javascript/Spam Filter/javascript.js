b38856bddbca93f7617b54ed941235a020fa5c06
Wambugu <stevenwambugu@proton.me>
2025-06-12 09:49:02 +0300

    elements are accessed from the html code using their ids and an addeventlistener is attached to the message button and an empty function as the callback

---
+++ javascript/Spam Filter/javascript.js
@@ -0,0 +1,5 @@
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
+
+checkMessageButton.addEventListener("click", () => {});
