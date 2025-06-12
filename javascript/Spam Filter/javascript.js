9650247447b358dd12f912bd151afc5d4bdeb065
Wambugu <stevenwambugu@proton.me>
2025-06-12 22:42:43 +0300

    a variable containing helpRegex is declared

--- javascript/Spam Filter/javascript.js
+++ javascript/Spam Filter/javascript.js
@@ -3,15 +3,19 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
+
+const denyList = [helpRegex];
+
 const isSpam = (msg) => helpRegex.test(msg);
 
 checkMessageButton.addEventListener("click", () => {
-    if (messageInput.value === "") {
-        alert("Please enter a message.");
-        return;
-      }
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
 
-      result.textContent = isSpam(messageInput.value) ? "Oh no! This looks like a spam message." : "This message does not seem to contain any spam.";
-      messageInput.value = "";
-
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
 });
