29a839096c5253b330f1e60eabf73308a5d638e4
Wambugu <stevenwambugu@proton.me>
2025-06-15 21:31:06 +0300

    a function that will enter the operational signs is entered

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,7 +1,11 @@
-let inputNumber = "0";
+let currentExpression = "0";
 
 const appendNumber = (number) => {
-    inputNumber += number;
-    document.getElementById("results").value = number;
+    currentExpression += number;
+    document.getElementById("results").value = currentExpression;
 }
 
+const appendOperator = (operator) => {
+    currentExpression += ` ${'operator'} `;
+    document.getElementById("results").value = currentExpression;
+}
