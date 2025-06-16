3d9b70e616c428df6930b6c7e8212229b46beb59
Wambugu <stevenwambugu@proton.me>
2025-06-16 14:24:00 +0300

    e and o character classes are set to match the letter and the corresponding number.

--- javascript/Spam Filter/javascript.js
+++ javascript/Spam Filter/javascript.js
@@ -5,7 +5,7 @@ const checkMessageButton = document.getElementById("check-message-btn");
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
 const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
-const stockRegex = /stock alert/i;
+const stockRegex = /st[o0]ck al[e3]rt/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
