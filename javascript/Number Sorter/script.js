de56f15f10afecc513207b9a10fc6342e9741029
Wambugu <stevenwambugu@proton.me>
2025-06-16 15:36:09 +0300

    an input value is declared and optional elements queried together

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -2,4 +2,8 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
-};
+  
+    const inputValues = document.getElementsByClassName("values-dropdown");
+  }
+
+sortButton.addEventListener("click", sortInputArray);
