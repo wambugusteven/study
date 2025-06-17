af60fa029436e3924d777dcb797f83b921047008
Wambugu <stevenwambugu@proton.me>
2025-06-17 15:18:00 +0300

    a map function is added that accepts dropdown as the parameter and returns dropdown.value

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -3,7 +3,7 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
   
-    const inputValues = [...document.getElementsByClassName("values-dropdown")];
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);;
   }
 
 sortButton.addEventListener("click", sortInputArray);
