95db6ecca2d21161436bf1852311fa5117462b98
Wambugu <stevenwambugu@proton.me>
2025-06-17 21:21:10 +0300

    an if loop is added that compares the array[j] and array[j+1]

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -23,6 +23,11 @@ const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
       console.log(array, array[j], array[j + 1]);
+      if (array[j] > array[j+1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      };
     }
   }
 }
