3c163686d0afb2fd554024ce5e8577709cc2f502
Wambugu <stevenwambugu@proton.me>
2025-06-17 21:38:32 +0300

    the sortedValues function is edited to call sectionSort function instead of bubbleSort

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 }
