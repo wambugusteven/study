c46d5d858dddbece19d8add5f6b759c02f0e73e5
Wambugu <stevenwambugu@proton.me>
2025-06-17 21:50:43 +0300

    an if statement is added inside the second for loop

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -38,7 +38,9 @@ const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     let minIndex = i;
    for (let j = i + 1; j < array.length; j++) {
-
+    if (array[j] < array[minIndex]) {
+      minIndex = j;
+    }
    }
   }
 };
