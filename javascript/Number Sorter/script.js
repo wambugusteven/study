0f3163f7b504ca563ae079afc0d9a2443a08c177
Wambugu <stevenwambugu@proton.me>
2025-06-17 21:59:37 +0300

    an insertionSort function is entered

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -38,7 +38,7 @@ const selectionSort = (array) => {
     let minIndex = i;
 
     for (let j = i + 1; j < array.length; j++) {
-      console.log(array, array[j], array[minIndex]);
+     
       if (array[j] < array[minIndex]) {
         minIndex = j;
       }
@@ -51,5 +51,7 @@ const selectionSort = (array) => {
 
   return array;
 }
+
+const insertionSort = (array) => {};
 
 sortButton.addEventListener("click", sortInputArray);
