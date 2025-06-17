d059528f423cf79fa2b18e036551318006912ae8
Wambugu <stevenwambugu@proton.me>
2025-06-17 16:13:22 +0300

    a function is declared that accepts a single array as the parameter

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -3,8 +3,11 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
   event.preventDefault();
   const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-  console.log(inputValues);
+ 
 }
+ 
+const updateUI = (array = []) => {
 
+}
 
 sortButton.addEventListener("click", sortInputArray);
