39658d803077c6a1e5805c1980ec68b01cb93bf5
Wambugu <stevenwambugu@proton.me>
2025-06-17 16:33:03 +0300

    a for loop is entered in the bubbleSort function

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -13,6 +13,10 @@ const updateUI = (array = []) => {
   })
 }
 
-const bubbleSort = (array) => {};
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+
+  }
+};
 
 sortButton.addEventListener("click", sortInputArray);
