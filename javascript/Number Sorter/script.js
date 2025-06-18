7a6a01b2f6442f643208c32904710ea8380e8549
Wambugu <stevenwambugu@proton.me>
2025-06-18 21:39:12 +0300

    a variable is declared and given array as the input

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -54,7 +54,8 @@ const selectionSort = (array) => {
 
 const insertionSort = (array) => {
   for (let i = 1; i < array.length; i++) {
-
+    const currValue = array[i];
+    let j = i - 1;
   }
 }
 
