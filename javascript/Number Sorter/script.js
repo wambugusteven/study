b3defa83c1c5b9a16db6e2d69b02240f1b06688c
Wambugu <stevenwambugu@proton.me>
2025-06-18 21:40:39 +0300

    a while loop is inserted inside th for loop

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -56,6 +56,11 @@ const insertionSort = (array) => {
   for (let i = 1; i < array.length; i++) {
     const currValue = array[i];
     let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+
+      j--;
+    }
   }
 }
 
