cb3ae211030e184bed4a742c68647d4a579f6f8f
Wambugu <stevenwambugu@proton.me>
2025-06-18 21:35:53 +0300

    the first for loop is inserted inside the insertionSort function

--- javascript/Number Sorter/script.js
+++ javascript/Number Sorter/script.js
@@ -53,7 +53,9 @@ const selectionSort = (array) => {
 }
 
 const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
 
+  }
 }
 
 sortButton.addEventListener("click", sortInputArray);
