301ba37e35eaa5a8b87d42d47026e8856e7a6dbc
Wambugu <stevenwambugu@proton.me>
2025-06-20 18:10:49 +0300

    a const is declared and template literal used to insert an id in the getElementById method

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -115,7 +115,12 @@ class ShoppingCart {
 
     const totalCountPerProduct = {};
     this.items.forEach((dessert) => {
-      
+      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
     })
+    const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`);
+
+
+    const currentProductCount = totalCountPerProduct[product.id];
+
   }
 };
