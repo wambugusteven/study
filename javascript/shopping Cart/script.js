45784c8e2d1313165a977fa1ed5665f442cf25de
Wambugu <stevenwambugu@proton.me>
2025-06-20 16:46:47 +0300

    a combination of items and push method is used to push the product into the items

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -111,6 +111,7 @@ class ShoppingCart {
   addItem(id, products) {
     const product = products.find((item) => item.id === id);
     const { name, price } = product;
+    this.items.push(product);
 
   }
 };
