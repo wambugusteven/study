d88c768f15f1b63abb6f60449b2aa426cf24bf2f
Wambugu <stevenwambugu@proton.me>
2025-06-20 19:27:24 +0300

    addition assignment operator and template literal is added

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -117,9 +117,16 @@ class ShoppingCart {
     this.items.forEach((dessert) => {
       totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
     })
-    const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`);
-     const currentProductCount = totalCountPerProduct[product.id];
-      
-     currentProductCount > 1 ? undefined : undefined;
+
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+
+    currentProductCount > 1 
+      ? currentProductCountSpan.textContent = `${currentProductCount}x`
+      : productsContainer.innerHTML += `
+      <div id="dessert${id}" class="product">
+
+      </div>
+      `;
   }
 };
