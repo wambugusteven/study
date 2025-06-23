4b87ec8f68d298ac1a929f0bb36141e7fc3f7727
Wambugu <stevenwambugu@proton.me>
2025-06-23 13:31:46 +0300

    a calculateTotal method is inserted to calculate the total of the products

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -132,6 +132,14 @@ class ShoppingCart {
       </div>
       `;
   }
+
+  getCounts() {
+    return this.items.length;
+  }
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+  }
+
 };
 
 const cart = new ShoppingCart();
@@ -141,6 +149,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
   (btn) => {
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
+      totalNumberOfItems.textContent = cart.getCounts();
     })
   }
 );
