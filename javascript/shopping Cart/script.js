a950de04da472b7b315eeecd3b8306fa8d2b219e
Wambugu <stevenwambugu@proton.me>
2025-06-23 15:06:10 +0300

    an if statement is added that checks if the array is empty and pops an alert message

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -138,7 +138,11 @@ class ShoppingCart {
   }
 
   clearCart() {
-   
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
   }
 
   calculateTaxes(amount) {
