4f8cc372e54185e2ce7f1e0a9074b53404a0975f
Wambugu <stevenwambugu@proton.me>
2025-06-23 15:05:12 +0300

    a clearCart method is created

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -137,6 +137,10 @@ class ShoppingCart {
     return this.items.length;
   }
 
+  clearCart() {
+   
+  }
+
   calculateTaxes(amount) {
     return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
