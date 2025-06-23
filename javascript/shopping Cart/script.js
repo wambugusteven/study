378f411ed5bf1d197bd866d6ccb30963e6f1c8e2
Wambugu <stevenwambugu@proton.me>
2025-06-23 14:46:29 +0300

    template literals and toFixed method is used to add a dollar sign and specify the number of decimal places

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -144,6 +144,7 @@ class ShoppingCart {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
     const tax = this.calculateTaxes(subTotal);
     this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
   }
 
 };
