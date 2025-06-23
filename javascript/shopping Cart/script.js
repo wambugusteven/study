588bedaca500e8953e6d9a1d70840593f091f596
Wambugu <stevenwambugu@proton.me>
2025-06-23 14:32:05 +0300

    an expression for calculating total which is the summation of subtotal and tax is established

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -143,6 +143,7 @@ class ShoppingCart {
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
     const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
   }
 
 };
