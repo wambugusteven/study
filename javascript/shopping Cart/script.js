e288ccc44e0ac67ce534704b22463b51b921d53b
Wambugu <stevenwambugu@proton.me>
2025-06-23 15:36:24 +0300

    confirm method is added that displays the message to the user

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -142,7 +142,7 @@ class ShoppingCart {
       alert("Your shopping cart is already empty");
       return;
     }
-
+    const isCartCleared = confirm("Are you sure you want to clear all items from your shopping cart?");
   }
 
   calculateTaxes(amount) {
