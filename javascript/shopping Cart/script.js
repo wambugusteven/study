f9f0609d19c357f927d17cd27e84e5f4c02bc187
Wambugu <stevenwambugu@proton.me>
2025-06-23 14:04:56 +0300

    an expression that will calculate the taxes is inserted inside the calculateTaxes method

--- javascript/shopping Cart/script.js
+++ javascript/shopping Cart/script.js
@@ -137,9 +137,9 @@ class ShoppingCart {
     return this.items.length;
   }
   calculateTaxes(amount) {
-  
+    return (this.taxRate / 100) * amount;
   }
-  
+
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
   }
