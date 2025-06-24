95e9c4d47d8b761f5aae17318a34de02d1b42aeb
Wambugu <stevenwambugu@proton.me>
2025-06-24 14:10:34 +0300

    the filter function is added a callback function with isNAN function

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -2,5 +2,5 @@ const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el));
-    const filtered = numbers.filter();
+    const filtered = numbers.filter(el => !isNaN(el));
 }
