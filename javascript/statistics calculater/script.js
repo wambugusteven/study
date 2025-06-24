3da7f73b45246b67004d9aeeec77817ccdf528d4
Wambugu <stevenwambugu@proton.me>
2025-06-24 14:13:47 +0300

    method chaining is applied for both map and filter methods

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -1,6 +1,7 @@
+
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
-    const numbers = array.map(el => Number(el));
-    const filtered = numbers.filter(el => !isNaN(el));
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 }
