779aa242bfebd31cfbe53993d8d50ccb771a847f
Wambugu <stevenwambugu@proton.me>
2025-06-25 14:38:10 +0300

    a sort method is added as the callback of the highest variable

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -11,11 +11,16 @@ const getMedian = (array) => {
 
 const getMode = (array) => {
   const counts = {};
-  array.forEach(el => counts[el] = counts[el] ? counts[el] + 1 : 1);
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
   if (new Set(Object.values(counts)).size === 1) {
     return null;
   }
-  const highest = Object.keys(counts);
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+
 }
 
 const calculate = () => {
