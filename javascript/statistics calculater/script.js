a4f348c0b2d2b565bbb2f0341b867cc281fe1575
Wambugu <stevenwambugu@proton.me>
2025-06-25 13:02:06 +0300

    pushed

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -2,6 +2,11 @@ const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
 
 const getMedian = (array) => {
   const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
 }
 
 const calculate = () => {
