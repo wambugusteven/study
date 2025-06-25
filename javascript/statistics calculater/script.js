e9859367679d40be9e4f37b89216e81cc623c766
Wambugu <stevenwambugu@proton.me>
2025-06-25 14:59:31 +0300

    a function is entered for calculating the range of numbers using spread operators

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -24,6 +24,10 @@ const getMode = (array) => {
   return mode.join(", ");
 }
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
