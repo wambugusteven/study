1207fdc9f7ce83af8c53c43101c664faf1bb76b2
Wambugu <stevenwambugu@proton.me>
2025-06-25 14:24:08 +0300

    the getMode function is created and all expressions included for calculating the mode

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -1,15 +1,18 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.toSorted((a, b) => a - b);
+  const sorted = array.sort((a, b) => a - b);
   const median =
-    sorted.length % 2 === 0
-      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
-      : sorted[Math.floor(sorted.length / 2)];
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
   return median;
 }
 
-const getMode = (array) => {};
+const getMode = (array) => {
+  const counts = {};
+  array.forEach(el => counts[el] = counts[el] ? counts[el] + 1 : 1);
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -18,6 +21,7 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
