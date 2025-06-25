921c90abbe26b21db8ce92d43abe4ee25d0e49ee
Wambugu <stevenwambugu@proton.me>
2025-06-25 15:00:19 +0300

    the range function is linked to the calculate function

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -36,8 +36,10 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
+  const range = getRange(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
    document.querySelector("#mode").textContent = mode;
+   document.querySelector("#range").textContent = range;
 }
