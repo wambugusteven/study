3f1d729e25e7c2fd1bbfde8624a03aa086c21823
Wambugu <stevenwambugu@proton.me>
2025-06-25 20:50:57 +0300

    a new variable is declared and array.map method attached to it

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -1,11 +1,11 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.toSorted((a, b) => a - b);
   const median =
-    array.length % 2 === 0
-      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
-      : sorted[Math.floor(array.length / 2)];
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
   return median;
 }
 
@@ -20,19 +20,29 @@ const getMode = (array) => {
   const highest = Object.keys(counts).sort(
     (a, b) => counts[b] - counts[a]
   )[0];
-  const mode = Object.keys(counts).filter((el) => counts[el] === counts[highest]);
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
   return mode.join(", ");
 }
 
 const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
-};
+}
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const differences = array.map(
+    el => el - mean
+  );
+
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
@@ -40,6 +50,6 @@ const calculate = () => {
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
-   document.querySelector("#mode").textContent = mode;
-   document.querySelector("#range").textContent = range;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
 }
