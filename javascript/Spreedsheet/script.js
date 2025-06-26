2d7d28790083ed913a3246def6220d886729b2b5
Wambugu <stevenwambugu@proton.me>
2025-06-26 07:55:53 +0300

    a div element is created inside the createLabel function and its classname and textContent set

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -1,4 +1,9 @@
 window.onload = () => {
     const container = document.getElementById("container");
-  
+      
+    const createLabel = (name) => {
+        const label = document.createElement("div");
+        label.className = "label";
+        label.textContent = name;
+    };
   }
