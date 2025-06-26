dd5b54a668e6c1ec970e3bb3e581d4492fdb4b94
Wambugu <stevenwambugu@proton.me>
2025-06-26 07:26:05 +0300

    the standard deviation function is added to the calculate function to perform once the button is clicked

--- javascript/statistics calculater/script.js
+++ javascript/statistics calculater/script.js
@@ -57,10 +57,13 @@ const calculate = () => {
   const mode = getMode(numbers);
   const range = getRange(numbers);
   const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
   document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
+
 }
