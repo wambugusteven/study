84023f1831a623d50998605f353d8e8647389e32
Wambugu <stevenwambugu@proton.me>
2025-06-27 17:39:48 +0300

    a function is created and an inner function created inside it

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -26,7 +26,12 @@ const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-
+  const elemValue = num => {
+    const inner = (character) => {
+      return idToText(character + num);
+    };
+    return inner;
+  }
  }
 
 window.onload = () => {
