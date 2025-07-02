db3969c3d18a3f071074554d996b6debe5d52ee4
Wambugu <stevenwambugu@proton.me>
2025-07-02 16:36:36 +0300

    the results returned is with a hasOwnProperty that returns true or false

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -39,7 +39,7 @@ const applyFunction = str => {
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
   const toNumberList = args => args.split(",").map(parseFloat);
   const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
-  return str2.replace(functionCall, () => {})
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) );
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
