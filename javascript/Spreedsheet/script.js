60fb1c653ced2fb79871a9b05a1c7366c6c369b3
Wambugu <stevenwambugu@proton.me>
2025-07-02 16:42:40 +0300

    a ternary operator to turn your .hasOwnProperty() call into the condition. If the object has the property, return the result of calling apply function

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -39,7 +39,7 @@ const applyFunction = str => {
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
   const toNumberList = args => args.split(",").map(parseFloat);
   const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
-  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) );
+  return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
@@ -54,6 +54,7 @@ const evalFormula = (x, cells) => {
   const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+
 }
 
 window.onload = () => {
