4572c51ab37498293e7402f8ca2f01c17f3243a2
Wambugu <stevenwambugu@proton.me>
2025-07-02 14:53:24 +0300

    an expression is added that checks whether the strings are equal

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -9,6 +9,8 @@ const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, ar
 
 const highPrecedence = str => {
   const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
 }
 
 const isEven = num => num % 2 === 0;
