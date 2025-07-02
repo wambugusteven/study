06be6b826888d428dab8f65f19bd1134c45874eb
Wambugu <stevenwambugu@proton.me>
2025-07-02 16:24:47 +0300

    a variable is declared that will find the sum of numbers given in bracket

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -35,6 +35,9 @@ const spreadsheetFunctions = {
 const applyFunction = str => {
   const noHigh = highPrecedence(str);
   const infix = /([\d.]+)([+-])([\d.]+)/;
+  const str2 = infixEval(noHigh, infix);
+  const functionCall =  /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+
 };
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
