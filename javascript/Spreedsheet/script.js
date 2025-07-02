1b5604eeaca86ab5eeb97ac857f584667a96e75a
Wambugu <stevenwambugu@proton.me>
2025-07-02 16:31:17 +0300

    a function that will execute the expressions without any case insensitive is added

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -37,7 +37,8 @@ const applyFunction = str => {
   const infix = /([\d.]+)([+-])([\d.]+)/;
   const str2 = infixEval(noHigh, infix);
   const functionCall =  /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
-
+  const toNumberList = args => args.split(",").map(parseFloat);
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()];
 };
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
