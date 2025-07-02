212c58db3e64d40f9aabe1c76d7b0cae07d2e48c
Wambugu <stevenwambugu@proton.me>
2025-07-02 16:17:01 +0300

    a function for aplying thr operators is declared

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -32,6 +32,8 @@ const spreadsheetFunctions = {
   median
 }
 
+const applyFunction = str => {};
+
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
