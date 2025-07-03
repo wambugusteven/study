fe3f00e488f7d7dc502363a0f4cccb7918395f89
Wambugu <stevenwambugu@proton.me>
2025-07-03 13:18:47 +0300

    a ternary opeator is used to evaluate some calculations

--- javascript/Spreedsheet/script.js
+++ javascript/Spreedsheet/script.js
@@ -55,7 +55,7 @@ const evalFormula = (x, cells) => {
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
   const functionExpanded = applyFunction(cellExpanded);
-
+  return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
 }
 
 window.onload = () => {
