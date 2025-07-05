d44417c912fd6ede7ff6863b7a6fc3a2c082baf3
Wambugu <stevenwambugu@proton.me>
2025-07-05 18:21:27 +0300

    a class is added with a constuctor inside it

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -13,3 +13,9 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = size => {
     return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 };
+
+class Player {
+    constructor() {
+
+    }
+}
