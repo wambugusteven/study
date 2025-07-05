eef76205f1a257877e1ccfde2d8d562e4dced0fb
Wambugu <stevenwambugu@proton.me>
2025-07-05 18:26:28 +0300

    a velocity object is added below the position object

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -20,5 +20,9 @@ class Player {
             x: proportionalSize(10),
             y: proportionalSize(400)
         }
+        this.velocity = {
+            x: 0,
+            y: 0,
+          }
     }
 }
