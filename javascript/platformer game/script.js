24577fc505b2ad72e74ddac682c7b42812603494
Wambugu <stevenwambugu@proton.me>
2025-07-07 15:51:40 +0300

    an animate function is created for clearing the graphics after playing

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -58,6 +58,12 @@ class Player {
 
 const player = new Player();
 
+const animate = () => {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+};
+
 const startGame = () => {
     canvas.style.display = "block";
    startScreen.style.display = "none";
