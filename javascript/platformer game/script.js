3c47350c587a0e2c0a9079e56ea2907e44d98ce0
Wambugu <stevenwambugu@proton.me>
2025-07-07 13:58:55 +0300

    a second if statement is added inside the first if statement and properties included

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -36,7 +36,10 @@ class Player {
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-    
+            if (this.position.y < 0) {
+                this.position.y = 0;
+                this.velocity.y = gravity;
+            };
         }
     }
 }
