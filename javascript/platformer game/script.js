cec32548dddd9ab8128f4fe84430ecf1a8701949
Wambugu <stevenwambugu@proton.me>
2025-07-08 06:57:03 +0300

    a switch method is used with sevral cases that defines the arrow keys

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -89,6 +89,27 @@ const movePlayer = (key, xVelocity, isPressed) => {
     }
 };
 
+switch (key) {
+  case "ArrowLeft":
+    keys.leftKey.pressed = isPressed;
+    if (xVelocity === 0) {
+      player.velocity.x = xVelocity;
+    }
+    player.velocity.x -= xVelocity;
+    break;
+  case "ArrowUp":
+  case " ":
+  case "Spacebar":
+    player.velocity.y -= 8;
+    break;
+    case "ArrowRight": 
+    keys.rightKey.pressed = isPressed;
+   if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x += xVelocity;
+}
+
 const startGame = () => {
     canvas.style.display = "block";
    startScreen.style.display = "none";
