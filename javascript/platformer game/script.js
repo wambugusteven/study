6a859c7238cf421e112fcc4c7b424861325dacb3
Wambugu <stevenwambugu@proton.me>
2025-07-10 14:37:39 +0300

    a variable for creating a new checkpoint is inserted using a map method

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -121,6 +121,10 @@ const checkpointPositions = [
    { x: 2900, y: proportionalSize(330), z: 2  },
     { x: 4800, y: proportionalSize(80), z: 3  }
 ];
+  
+const checkpoints = checkpointPositions.map(
+  (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z)
+);
 
 const animate = () => {
   requestAnimationFrame(animate);
