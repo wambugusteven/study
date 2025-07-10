619238a5bcd0e58f44efb8700ee348da569fa542
Wambugu <stevenwambugu@proton.me>
2025-07-10 12:34:25 +0300

    the platform detection array is established with rules set into it

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -133,6 +133,13 @@ const animate = () => {
       player.velocity.y = 0;
       return;
    }
+
+   const platformDetectionRules = [
+    player.position.x >= platform.position.x - player.width / 2,
+    player.position.x <= platform.position.x + platform.width - player.width / 3,
+    player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+   ];
  });
 
 }
