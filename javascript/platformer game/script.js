6eb3275b449df2852b5d435716481a82564c032c
Wambugu <stevenwambugu@proton.me>
2025-07-10 14:34:34 +0300

    an array for checking the point position is created

--- javascript/platformer game/script.js
+++ javascript/platformer game/script.js
@@ -116,7 +116,11 @@ const platforms = platformPositions.map(
   (platform) => new Platform(platform.x, platform.y)
 );
 
-
+const checkpointPositions = [
+  { x: 1170, y: proportionalSize(80), z: 1 },
+   { x: 2900, y: proportionalSize(330), z: 2  },
+    { x: 4800, y: proportionalSize(80), z: 3  }
+];
 
 const animate = () => {
   requestAnimationFrame(animate);
