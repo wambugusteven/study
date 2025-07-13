3b7cb5ae2b4c7cd24b8446e07a55d49f7bbb9a98
Wambugu <stevenwambugu@proton.me>
2025-07-13 14:23:01 +0300

    a function that clears all the data and activates disabled is inserted

--- javascript/Dice game/script.js
+++ javascript/Dice game/script.js
@@ -76,6 +76,17 @@ const getHighestDuplicates = (arr) => {
   updateRadioOption(5, 0);
 };
 
+const resetRadioOptions = () => {
+  scoreInputs.forEach((input) => {
+    input.disabled = true;
+    input.checked = false;
+  });
+
+  scoreSpans.forEach((span) => {
+    span.textContent = "";
+  });
+};
+
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score")
