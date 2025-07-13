32a9f0e8cf6a366e9d4d192089057965f7dfa464
Wambugu <stevenwambugu@proton.me>
2025-07-13 13:39:32 +0300

    an if statement is added that allows the player to only roll the dice thrice before displaying an message

--- javascript/Dice game/script.js
+++ javascript/Dice game/script.js
@@ -30,7 +30,12 @@ const rollDice = () => {
 };
 
 rollDiceBtn.addEventListener("click", () => {
-  rollDice();
+  if (rolls === 3) {
+    alert("You have made three rolls this round. Please select a score")
+  } else {
+    rolls++;
+    rollDice();
+  };
 });
 
 rulesBtn.addEventListener("click", () => {
