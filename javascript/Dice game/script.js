90364724d6991201c061efb7b00f7d503e9a23a3
Wambugu <stevenwambugu@proton.me>
2025-07-13 14:12:49 +0300

    a function that counts how many times each number is found in the array is created

--- javascript/Dice game/script.js
+++ javascript/Dice game/script.js
@@ -50,6 +50,42 @@ rollDiceBtn.addEventListener("click", () => {
   };
 });
 
+const getHighestDuplicates = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+
+  let highestCount = 0;
+
+  for (const num of arr) {
+    const count = counts[num];
+    if (count >= 3 && count > highestCount) {
+      highestCount = count;
+    }
+    if (count >= 4 && count > highestCount) {
+      highestCount = count;
+    }
+  }
+
+  const sumOfAllDice = arr.reduce((a, b) => a + b, 0);
+
+  if (highestCount >= 4) {
+    updateRadioOption(1, sumOfAllDice);
+  }
+
+  if (highestCount >= 3) {
+    updateRadioOption(0, sumOfAllDice);
+  }
+
+  updateRadioOption(5, 0);
+};
+
 rulesBtn.addEventListener("click", () => {
     isModalShowing = !isModalShowing;
   
