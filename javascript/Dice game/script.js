ab86cb93e84b41e59f02fb33a80df9af3f1cd8be
Wambugu <stevenwambugu@proton.me>
2025-07-16 16:47:32 +0300

    an algorithm that checks for the presence of a straight is established in a function and called inside the rollDiceBtn

--- javascript/Dice game/script.js
+++ javascript/Dice game/script.js
@@ -47,6 +47,7 @@ const updateScore = (selectedValue, achieved) => {
   scoreHistory.innerHTML += `<li>${achieved} : ${selectedValue}</li>`;
 };
 
+
 const getHighestDuplicates = (arr) => {
   const counts = {};
 
@@ -100,6 +101,25 @@ const detectFullHouse = (arr) => {
   updateRadioOption(5, 0);
 };
 
+const checkForStraights = (arr) => {
+  const sortedNumbersArr = arr.sort((a, b) => a - b);
+  const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+  const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+  const smallStraightsArr = ["1234", "2345", "3456"];
+  const largeStraightsArr = ["12345", "23456"];
+
+  if (smallStraightsArr.some(straight => uniqueNumbersStr.includes(straight))) {
+    updateRadioOption(3, 30);
+  }
+
+  if (largeStraightsArr.includes(uniqueNumbersStr)) {
+    updateRadioOption(4, 40);
+  }
+
+  updateRadioOption(5, 0);
+};
+
 const resetRadioOptions = () => {
   scoreInputs.forEach((input) => {
     input.disabled = true;
@@ -140,6 +160,7 @@ rollDiceBtn.addEventListener("click", () => {
     updateStats();
     getHighestDuplicates(diceValuesArr);
     detectFullHouse(diceValuesArr);
+    checkForStraights(diceValuesArr); 
   }
 });
 
