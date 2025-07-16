9752b84e559e0695828e8d458a091d365dfb02f8
Wambugu <stevenwambugu@proton.me>
2025-07-16 16:44:23 +0300

    a detectFullHouse function is inserted that checks whether the user has rolled several times

--- javascript/Dice game/script.js
+++ javascript/Dice game/script.js
@@ -83,7 +83,22 @@ const getHighestDuplicates = (arr) => {
   updateRadioOption(5, 0);
 };
 
+const detectFullHouse = (arr) => {
+  const counts = {};
 
+  for (const num of arr) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  const hasThreeOfAKind = Object.values(counts).includes(3);
+  const hasPair = Object.values(counts).includes(2);
+
+  if (hasThreeOfAKind && hasPair) {
+    updateRadioOption(2, 25);
+  }
+
+  updateRadioOption(5, 0);
+};
 
 const resetRadioOptions = () => {
   scoreInputs.forEach((input) => {
@@ -124,7 +139,7 @@ rollDiceBtn.addEventListener("click", () => {
     rollDice();
     updateStats();
     getHighestDuplicates(diceValuesArr);
-  
+    detectFullHouse(diceValuesArr);
   }
 });
 
