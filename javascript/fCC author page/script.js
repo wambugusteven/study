3543f1b09cd830cc2d37b425f759e8d9bc939782
Wambugu <stevenwambugu@proton.me>
2025-07-18 17:17:05 +0300

    a function is added that will load the rest of the authors

--- javascript/fCC author page/script.js
+++ javascript/fCC author page/script.js
@@ -15,6 +15,12 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     console.error(`There was an error: ${err}`);
   });
 
+  const fetchMoreAuthors = () => {
+    startingIndex += 8;
+    endingIndex += 8; 
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  };
+
   const displayAuthors = (authors) => {
     authors.forEach(({ author, image, url, bio }, index) => {
         authorContainer.innerHTML += `
