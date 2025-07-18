07de79fbe0a41670103cdcb356c0ea222281920b
Wambugu <stevenwambugu@proton.me>
2025-07-18 15:21:57 +0300

    a catch method used to handle errors incase the promise gets rejected is added

--- javascript/fCC author page/script.js
+++ javascript/fCC author page/script.js
@@ -6,3 +6,6 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
 .then((data) => {
     console.log(data);
   })
+  .catch((err) => {
+    console.error(`There was an error: ${err}`);
+  });
