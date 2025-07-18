aacb089319c94d78cc17741b420509410fd3ba33
Wambugu <stevenwambugu@proton.me>
2025-07-18 15:30:54 +0300

    a function that will display the authors on the screen is created

--- javascript/fCC author page/script.js
+++ javascript/fCC author page/script.js
@@ -9,7 +9,12 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
 .then((res) => res.json())
 .then((data) => {
     console.log(data);
+    displayAuthors();
   })
   .catch((err) => {
     console.error(`There was an error: ${err}`);
   });
+
+  const displayAuthors = (authors) => {
+    authors.forEach({ author, image, url, bio })
+  };
