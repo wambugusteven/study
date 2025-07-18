573c81639f9929aa9ed725e26e3b1adfb79ef29b
Wambugu <stevenwambugu@proton.me>
2025-07-18 16:00:49 +0300

    a paragraph element is added that interpolates bio which contain information about the aouthor

--- javascript/fCC author page/script.js
+++ javascript/fCC author page/script.js
@@ -21,6 +21,7 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
           <div id="${index}" class="user-card">
              <h2 class="author-name">${author}</h2>
              <img class="user-img" src="${image}" alt="${author} avatar" />
+             <p class="bio">${bio}</p>
           </div>
         `;
       });
