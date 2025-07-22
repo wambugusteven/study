5f3b633f1323fc842998f4ecd1cb0adc8f5e75f2
Wambugu <stevenwambugu@proton.me>
2025-07-22 06:21:30 +0300

    an object that holds information abot catergory and className is created

--- javascript/leader board/script.js
+++ javascript/leader board/script.js
@@ -5,6 +5,17 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+    299: { category: "Career Advice", className: "career" },
+    409: { category: "Project Feedback", className: "feedback" },
+    417: { category: "freeCodeCamp Support", className: "support" },
+    421: { category: "JavaScript", className: "javascript" },
+    423: { category: "HTML - CSS", className: "html-css" },
+    424: { category: "Python", className: "python" },
+    432: { category: "You Can Do This!", className: "motivation" },
+    560: { category: "Backend Development", className: "backend" },
+};
+
 const timeAgo = (time) => {
     const currentTime = new Date();
     const lastPost = new Date(time);
@@ -61,7 +72,7 @@ const showLatestPosts = (data) => {
           <td>
           ${posts_count - 1}
           </td>
-          <td>${views}</td>
+          <td>${viewCount(views)}</td>
           <td>${timeAgo(bumped_at)}</td>
           </tr>
         `
