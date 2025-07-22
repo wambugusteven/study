af410f83a5000df5152e6d3a491b66d3c33a2ed6
Wambugu <stevenwambugu@proton.me>
2025-07-22 05:59:31 +0300

    a table is created that will display the results of the calling functions

--- javascript/leader board/script.js
+++ javascript/leader board/script.js
@@ -5,14 +5,19 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const timeAgo = time => {
+    const currentTime = new Date();
+    const lastPost = new Date(time);
+  };
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
-    const data = await res.json();  
+    const data = await res.json();
+    showLatestPosts(data);
   } catch (err) {
     console.log(err);
   }
-  showLatestPosts(data);
 };
 fetchData();
 
@@ -22,7 +27,16 @@ const showLatestPosts = (data) => {
     postsContainer.innerHTML = topics.map((item) => {
         const { id, title, views, posts_count, slug, posters, category_id, bumped_at } = item;
         return `
-          <tr></tr>
+          <tr>
+          <td>
+          <p class="post-title">${title} </p></td>
+          <td></td>
+          <td>
+          ${posts_count - 1}
+          </td>
+          <td>${views}</td>
+          <td></td>
+          </tr>
         `
     }).join('')
 };
