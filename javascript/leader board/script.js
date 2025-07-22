c011c540f5228bc8bccb5946d251f6129046d1ad
Wambugu <stevenwambugu@proton.me>
2025-07-22 05:06:11 +0300

    a try and catch method is inserted to handle any kind of error

--- javascript/leader board/script.js
+++ javascript/leader board/script.js
@@ -2,6 +2,16 @@ const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
 const postsContainer = document.getElementById("posts-container");
 
-const fetchData = async () => {};
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest);
+        const data = await res.json();
+      } catch (err) {
+        console.log(err)
+      }
+    };
+    
+    fetchData();
