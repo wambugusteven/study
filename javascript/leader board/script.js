8304ebb456a67caf0a6b816d8ea0e8eb2aaf898a
Wambugu <stevenwambugu@proton.me>
2025-07-24 05:05:33 +0300

    the description part is converted into a link that should open on a new webpage

--- javascript/leader board/script.js
+++ javascript/leader board/script.js
@@ -101,7 +101,7 @@ const showLatestPosts = (data) => {
         return `
           <tr>
       <td>
-        <p class="post-title">${title}</p>
+       <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
         ${forumCategory(category_id)}
       </td>
       <td>
