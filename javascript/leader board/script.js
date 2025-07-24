0ac73b612edacdfcaa0e90979253cccb432b51f8
Wambugu <stevenwambugu@proton.me>
2025-07-24 04:57:52 +0300

    a function that checks for posters and users is established and code included

--- javascript/leader board/script.js
+++ javascript/leader board/script.js
@@ -67,6 +67,21 @@ const timeAgo = (time) => {
     return views;
   };
 
+  const avatars = (posters, users) => {
+    return posters
+      .map((poster) => {
+        const user = users.find((user) => user.id === poster.user_id);
+        if (user) {
+          const avatar = user.avatar_template.replace(/{size}/, 30);
+          const userAvatarUrl = avatar.startsWith("/user_avatar/")
+            ? avatarUrl.concat(avatar)
+            : avatar;
+          return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+        }
+      })
+      .join("");
+  };
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
