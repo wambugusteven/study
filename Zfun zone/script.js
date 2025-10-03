893564b10df9d6a751c00d5afbadfc256bf892e3
steven wambugu <stevenwambugu@proton.me>
2025-10-03 20:20:09 +0300

    several modifications are done on the appearance of the note taking webpage

--- Zfun zone/script.js
+++ Zfun zone/script.js
@@ -1,30 +1,16 @@
-const ntn = document.getElementById("dsn-scr");
+const ntn = document.getElementById("bt");
 const ntn1 = document.getElementById("dsn-scr1");
-const button1 = document.getElementById("btn1");
+const button1 = document.getElementById("btn-td");
 const button2 = document.getElementById("btn2");
 
-let noteNumber = 0;
-for (let i = 1; i <= 1; i++) {
-    noteNumber = noteNumber + 1; 
+const mdk = () => {
+    ntn.innerHTML = new Date();
 }
 
-const row = {
-    noteBlock: `
-        <div class="dis">
-           <h1>${noteNumber}</h1>
-           <input type="text" />
-        </div>
-    `
-};
-
-const noteAdd = () => {
-     ntn.innerHTML = row[0];
-}
-button1.addEventListener("click", noteAdd);
+button1.addEventListener("click", mdk);
 
 const dateDis = () => {
     ntn1.innerHTML = "Love is a scam 😜";
 }
 button2.addEventListener("click", dateDis);
 
-ntn.innerHTML += row[0];
