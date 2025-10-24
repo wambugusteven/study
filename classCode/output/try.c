2714f3b9f5d6c020668d6bd7243c6f80a89fefee
steven wambugu <stevenwambugu@proton.me>
2025-10-24 05:36:34 +0300

    c code basics is learned

---
+++ classCode/output/try.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int main () 
+ {
+  int num = 18, sum;
+  float dec = 4.98;
+  char big = 'S';
+ 
+ printf("This is my age: %d\n Average number: %f\n", num, dec);
+ printf("%c\n", big);
+ sum = num + dec;
+ printf("the sum of my age and average number is: %d\n", sum);
+  return 0;
+ }
