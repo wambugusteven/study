65d889d3a8fc1e5814bdd76258692eaa866271ee
steven wambugu <stevenwambugu@proton.me>
2025-10-24 06:51:05 +0300

    a format specifier for text is included

--- classCode/output/try.c
+++ classCode/output/try.c
@@ -8,12 +8,14 @@
   int studentID = 15;
   float studentFee = 75.25;
 char studentGrade = 'B';
+ char name[] = "Steven";
  
  printf("This is my age: %d\n Average number: %f\n", num, dec);
  printf("%c\n", big);
  sum = num + dec;
  printf("the sum  is: %d\n", sum);
- printf("Student id: %d\n Student fee: %f\n Student grade: %c", studentID,  studentFee, studentGrade);
+ printf("Student id: %d\n Student fee: %f\n Student grade: %c\n", studentID,  studentFee, studentGrade);
+ printf("My name: %s\n", name);
   return 0;
  }
 
