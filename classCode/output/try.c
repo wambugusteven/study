f64a3fbde7e78abeacb1b1aa4b6acfecd6e0aa98
steven wambugu <stevenwambugu@proton.me>
2025-10-24 09:40:36 +0300

    extended types of data types is added

--- classCode/output/try.c
+++ classCode/output/try.c
@@ -19,6 +19,22 @@ char studentGrade = 'B';
  printf("My name: %s\n", name);
  printf("%.3f\n", div);
 
+ short int small = -100;                    
+unsigned int count = 25;                    
+long int big = 1234567890;                  
+long long int veryBig = 9223372036854775807; 
+unsigned long long int huge = 18446744073709551615U; 
+long double precise = 3.141592653589793238L; 
+
+printf("Normal int: %d\n", normalInt);
+printf("Normal double: %lf\n", normalDouble);
+printf("Small: %hd\n", small);
+printf("Count: %u\n", count);
+printf("Big: %ld\n", big);
+printf("Very Big: %lld\n", veryBig);
+printf("Huge: %llu\n", huge);
+printf("Precise: %Lf\n", precise);
+
   return 0;
  }
 
