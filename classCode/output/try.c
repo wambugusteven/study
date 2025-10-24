011c82b4eef148feda31fa7dd40d6467a2c7e6cd
steven wambugu <stevenwambugu@proton.me>
2025-10-24 06:41:59 +0300

    more elements added

--- classCode/output/try.c
+++ classCode/output/try.c
@@ -5,10 +5,15 @@
   int num = 18, sum;
   float dec = 4.98;
   char big = 'S';
+  int studentID = 15;
+  float studentFee = 75.25;
+char studentGrade = 'B';
  
  printf("This is my age: %d\n Average number: %f\n", num, dec);
  printf("%c\n", big);
  sum = num + dec;
- printf("the sum of my age and average number is: %d\n", sum);
+ printf("the sum  is: %d\n", sum);
+ printf("Student id: %d\n Student fee: %f\n Student grade: %c", studentID,  studentFee, studentGrade);
   return 0;
  }
+
