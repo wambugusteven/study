973bffade897fb44bb986aa534522ae8a7b30ce0
steven wambugu <stevenwambugu@proton.me>
2025-11-05 09:55:14 +0300

    the code is cleaned up and several modification done to prevent repetition of words

--- classCode/bsics/afe.c
+++ classCode/bsics/afe.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
- int main ()
- {
-   int Aage = 18;
+
+int main() {
+    int Aage = 18;
     int age;
     int count;
     int myNumber[7];
@@ -9,57 +9,66 @@
     int MyNum[] = {10, 20, 30, 40, 50, 60, 90, 100};
     int i;
     int LENGTH = sizeof(MyNum) / sizeof(MyNum[0]);
+    int lowestNumber = MyNum[0];
 
     printf("Enter your age: ");
     scanf("%d", &age); 
+
     printf("Enter count: ");
     scanf("%d", &count);
 
+    // Age check
     if (age >= Aage) {
         printf("Old enough to vote!\n");
     } else {
         printf("Still have a long way to go.\n");
     }
-  
-  while (count <= 6) {
-    if(count <6) {
-      printf("Dice number less than 6?!\n");
-    } else {
-      printf("Dice number is 6.\n");
+
+   
+    while (count <= 6) {
+        if (count < 6) {
+            printf("Dice number less than 6?!\n");
+        } else {
+            printf("Dice number is 6.\n");
+        }
+        count++;
     }
 
-    count++;
-  }
-
-  switch (age >= Aage) {
-    case 1:
-    printf("You're an adult.\n");
-    break;
-    case 0:
-    printf("You're a child!\n");
-  }
-  
-  myNumber[0] = 2;
-  myNumber[1] = 3;
-  myNumber[2] = -4;
-  myNumber[3] = 5;
-  myNumber[4] = 6;
-  myNumber[5] = 0;
-  myNumber[6] = 9;
-
-  for (int j = 0; j < length; j++) {
-    if(myNumber[j] < length) {
-      continue;
+   
+    switch (age >= Aage) {
+        case 1:
+            printf("You're an adult.\n");
+            break;
+        case 0:
+            printf("You're a child!\n");
+            break;
     }
-    if(myNumber[j] == 0) {
-      break;
-    }
-    printf("%d\n", myNumber[j]);
-  }
 
-  for (i = 0; i < LENGTH; i++) {
-    printf("%d\n", MyNum[i]);
-  }
-  
-  return 0;
-   }
+    
+    int values[] = {2, 3, -4, 5, 6, 0, 9};
+    for (int j = 0; j < length; j++) {
+        myNumber[j] = values[j];
+    }
+
+   
+    printf("\nmyNumber values:\n");
+    for (int j = 0; j < length; j++) {
+        if (myNumber[j] == 0) {
+            break;
+        }
+        printf("%d\n", myNumber[j]);
+    }
+
+    
+    printf("\nMyNum values:\n");
+    for (i = 0; i < LENGTH; i++) {
+        printf("%d\n", MyNum[i]);
+        if (lowestNumber > MyNum[i]) {
+            lowestNumber = MyNum[i];
+        }
+    }
+
+    printf("And the lowest Number is: %d\n", lowestNumber);
+
+    return 0;
+}
